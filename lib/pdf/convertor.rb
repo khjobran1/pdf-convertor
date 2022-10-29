2f8ddce4b363f1a5f417afecba8e6378c2c8c459
Khaled Jobran <khaled.j@hungerstation.com>
2022-10-29 06:01:12 +0300

    initial commit

---
+++ lib/pdf/convertor.rb
@@ -0,0 +1,8 @@
+require "pdf/convertor/version"
+
+module Pdf
+  module Convertor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/pdf/convertor_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Pdf::Convertor do
+  it "has a version number" do
+    expect(Pdf::Convertor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
